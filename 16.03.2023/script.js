ea3e8711a2b8aca35454e48a40d09524960b924f
Madina <muxtorovamadinabonu416@gmail.com>
2023-03-23 15:15:11 +0500

    habar qoshildi

---
+++ 16.03.2023/script.js
@@ -0,0 +1,19 @@
+const countryAPI = 'country.json';
+const getData = (resurse)=>{
+    fetch(resurse)
+    .then((resurse)=>{
+        
+    })
+}
+getData(countryAPI)
+// fetch(countryAPI)
+//     .then((request)=>{
+//         const data = request.json();
+//         return data;
+//     })
+//     .then((aaa)=>{
+//         console.log(aaa)
+//     })
+//     .catch(()=>{
+
+//     })
